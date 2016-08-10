72e7380a88531d5a82b6dfd08ab010f967e241c0
iciuta <ionut.ciuta@endava.com>
2016-08-10 18:05:01 +0300

    spring project

---
+++ wiki-explorer/src/main/java/com/endava/wikiexplorer/WikiExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.endava.wikiexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiExplorerApplication.class, args);
+	}
+}
