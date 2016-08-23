c986a89fddf8b1c979a08c50e27c9a64d22b61fc
Andrei Ciurea <aciurea@endava.com>
2016-08-23 11:06:07 +0300

    titles=title

--- wiki-explorer/src/main/java/com/endava/wikiexplorer/repository/Impl/QueryRepositoryImpl.java
+++ wiki-explorer/src/main/java/com/endava/wikiexplorer/repository/Impl/QueryRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.Iterator;
  */
 public abstract class QueryRepositoryImpl implements QueryRepository {
     @Override
-    public Query findByTitles(String titles) {
+    public Query findByTitle(String titles) {
         Iterator iterator= (Iterator) findAll();
         while(iterator.hasNext()){
             Query query= (Query) iterator.next();
--- wiki-explorer/src/main/java/com/endava/wikiexplorer/repository/QueryRepository.java
+++ wiki-explorer/src/main/java/com/endava/wikiexplorer/repository/QueryRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QueryRepository extends CrudRepository<Query, Integer> {
 
-    Query findByTitles(String titles);
+    Query findByTitle(String titles);
 }
